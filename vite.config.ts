f9f16f1c7630360005a04c50135b79ff7e7b1741
ShawTim <shawtim.liu@gmail.com>
2026-07-17 03:53:58 +0800

    fix: use relative base path ./ for both custom domain and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   server: { port: 3000, open: true },
   assetsInclude: ["**/*.glb", "**/*.gltf"],
 });
